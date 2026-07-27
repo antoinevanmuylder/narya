dcfd1efc19b52f72597a62543fd7efd77f80996f
antoinevanmuylder <antvanmul@hotmail.com>
2026-07-27 15:22:52 +0200

    comment install commands

--- install-cmds.sh
+++ install-cmds.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-dune build narya.opam
-opam install . --deps-only
-dune build @install
+dune build narya.opam # generates the narya.opam metadata file from dune-project
+opam install . --deps-only # fetches from web and compile deps, add them to opam switch
+dune build @install # build project
 dune test
-dune install
+dune install # move compiled files to opam switch
